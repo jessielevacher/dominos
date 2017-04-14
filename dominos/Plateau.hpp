7738d240e032bf77c9390808acc1dd733dc33baf
maaareme <mareme.thiam@insa-rouen.fr>
2017-04-14 10:16:40 +0200

    Header de la classe Plateau.

--- dominos/Plateau.hpp
+++ dominos/Plateau.hpp
@@ -1,6 +1,26 @@
 /*
  * Plateau.hpp
  *
- *  Created on: 14 avril 2017
+ *  Created on: 14 avr. 2017
  *      Author: mthiam
  */
+
+#ifndef PLATEAU_HPP_
+#define PLATEAU_HPP_
+#include <string>
+using namespace std;
+
+class Plateau {
+private :
+	int valInf;
+	int valSup;
+public :
+	Plateau();
+	int getValInf() const;
+	void setValInf(int);
+	int getValSup();
+	void setValSup(int);
+	void ajouterDominoPlateau(Domino, int);
+	void modifierBorne(int);
+	bool verifierCompatiblite(int);
+};
