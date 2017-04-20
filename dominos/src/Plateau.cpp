31541f21b3fa285819fb8f4d343a439ea827d316
lmanneheut <lea.manneheut@insa-rouen.fr>
2017-04-20 13:00:54 +0200

    classe Plateau
    ajout du constructeur et de la simple définition des méthode pour
    pouvoir compiler le tout

--- dominos/src/Plateau.cpp
+++ dominos/src/Plateau.cpp
@@ -6,5 +6,51 @@
  */
 
 
+#include "Plateau.hpp"
+#include <vector>
+
+int Plateau::getValInf() const{
+	return valInf;
+}
+
+/*Plateau::Plateau(Domino dom){
+	valInf=dom.getValInf();
+	valSup=dom.getValSup();
+	dominosPlateau=new vector<Domino>();
+	dominosPlateau->at(0)=dom;
+}*/
+
+Plateau::Plateau(){
+	valInf=NULL;
+	valSup=NULL;
+	dominosPlateau=new vector<Domino>();
+
+}
+
+void Plateau::setValInf(int vI){
+	valInf=vI;
+}
+
+int Plateau::getValSup(){
+	return valSup;
+}
+
+void Plateau::setValSup(int vS){
+	valSup=vS;
+}
+
+void Plateau::ajouterDominoPlateau(Domino d, int val){
+
+}
+
+void Plateau::modifierBorne(int val){
+
+}
+
+bool Plateau::verifierCompatiblite(int val){
+	bool a="True";
+	return a;
+}
+
 
 
