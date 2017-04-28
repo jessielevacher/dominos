57aa70c452d5d09dcef20537d7e5349f7f2e5ebb
lguillard <laetitia.guillard.lg@gmail.com>
2017-04-28 09:37:16 +0200

    test2

---
+++ dominos/src/test2.cpp
@@ -0,0 +1,35 @@
+/*
+ * test2.cpp
+ *
+ *  Created on: 28 avr. 2017
+ *      Author: lguillard
+ */
+
+#include "JeuDomino.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+	JeuDomino jeu;
+
+	cout << " bornes du plateau: " << jeu.plateau.getValInf() << "-" << jeu.plateau.getValSup() << endl;
+	cout << " domino déposer sur le plateau en premier: " <<  	jeu.plateau.getDominosPlateau()->at(0)  <<endl;
+
+	Domino d = jeu.joueur.getListeDominos()->at(4);
+	Domino d1(1,3);
+
+	for (int i=0;i<jeu.joueur.getNbDominosRestants();i++)
+		cout << jeu.joueur.getListeDominos()->at(i) << endl;
+
+	cout << "--------" << endl;
+
+	jeu.joueur.deposerDomino(jeu.plateau, jeu.pioche);
+
+	for (int i=0;i<jeu.joueur.getNbDominosRestants();i++)
+			cout << jeu.joueur.getListeDominos()->at(i) << endl;
+
+
+
+}
+
+
