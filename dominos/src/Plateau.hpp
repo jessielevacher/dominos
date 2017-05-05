1eaa85d7d402190dffc0660fab04d99002babd7b
maaareme <mareme.thiam@insa-rouen.fr>
2017-05-05 09:24:32 +0200

    Documentation Plateau.hpp

--- dominos/src/Plateau.hpp
+++ dominos/src/Plateau.hpp
@@ -2,7 +2,7 @@
  * Plateau.hpp
  *
  *  Created on: 14 avr. 2017
- *      Author: mthiam
+ *      Author: {Mareme}
  */
 
 #ifndef PLATEAU_HPP_
@@ -12,24 +12,95 @@
 #include <string>
 #include "Domino.hpp"
 using namespace std;
+/*!
+ * \class Plateau
+ * \brief Classe représentant le plateau de jeu
+ *
+ *  La classe gère les dominos joués par les joueurs
+ */
 
 class Plateau{
 private :
-	int valInf;
-	int valSup;
-	vector<Domino>* dominosPlateau;
+	int valInf; /*!< Valeur inférieure du plateau*/
+	int valSup;/*!< Valeur supérieure du plateau*/
+	vector<Domino>* dominosPlateau; /*!< Vecteur des dominos posés sur le plateau*/
 
 public :
+	/*!
+	*  \brief Constructeur
+	*
+	*  Constructeur de la classe Plateau
+	*
+	*/
 	Plateau();
+
+	/*!
+	* \brief Permet d'accéder à la valeur inférieure du plateau
+	*
+	* \return la valeur inférieure du plateau
+	*/
 	int getValInf() const;
+
+	/*!
+	* \brief Permet de modifier à la valeur inférieure du plateau
+	*
+	* \param nouvelle valeur inférieure du plateau
+	*/
 	void setValInf(int);
+
+	/*!
+	* \brief Permet d'accéder à la valeur supérieure du plateau
+	*
+	* \return la valeur supérieure du plateau
+	*/
 	int getValSup();
+
+	/*!
+	* \brief Permet de modifier à la valeur supérieure du plateau
+	*
+	* \param nouvelle valeur supérieure du plateau
+	*/
 	void setValSup(int);
+
+	/*!
+	* \brief Permet d'accéder aux dominos posés sur le plateau
+	*
+	* \return le vecteur de dominos du plateau
+	*/
 	vector<Domino>* getDominosPlateau();
+
+	/*!
+	* \brief Permet de poser un domino à la fin du vecteur de dominos du plateau
+	*
+	* \param domino à poser
+	*/
 	void setBackDominosPlateau(const Domino);
+
+	/*!
+	* \brief Permet de poser un domino au début du vecteur de dominos du plateau
+	*
+	* \param domino à poser
+	*/
 	void setBeginDominosPlateau(const Domino);
+
+	/*!
+	* \brief Permet d'ajouter un domino sur le plateau en faisant la jonction avec un côté du domino
+	*
+	* \param domino à poser
+	* \param côté du domino que l'on veut poser
+	*/
 	void ajouterDominoPlateau(Domino, int);
+
+	/*!
+	* \brief Vérifie qu'il est possible de poser le domino choisi suivant le côté choisi
+	*
+	* \param côté du domino avec lequel on fait la jonction
+	*/
 	bool verifierCompatibilite(int);
+
+	/*!
+	* \brief Affiche les dominos du plateau sur la console
+	*/
 	void afficherPlateau();
 };
 
