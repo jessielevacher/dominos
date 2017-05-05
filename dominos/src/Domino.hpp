06046adfcb54ec8cecfecb4a96798dede500e82e
jessielevacher <jessie1403@msn.com>
2017-05-05 08:31:09 +0200

    "documentation classe Pioche et Domino

--- dominos/src/Domino.hpp
+++ dominos/src/Domino.hpp
@@ -8,24 +8,75 @@
 #ifndef DOMINO_HPP_
 #define DOMINO_HPP_
 
+/*!
+ * \file Domino.hpp
+ * \brief Domino
+ * \author {Léa Manneheut, Jessie Levacher}
+ */
+
 #include <vector>
 #include <iostream>
 #include <cmath>
 #include <cstdio>
 #include <string>
+
 using namespace std;
+
+/*!
+ * \class Domino
+ * \brief Classe représentant un domino
+ *
+ *  La classe gère les dominos
+ */
 
 class Domino{
 	private:
-		int valInf;
-		int valSup;
+		int valInf; /*!< Valeur inférieure du domino*/
+		int valSup; /*!< Valeur supérieure du domino*/
 
 	public:
-		Domino(const int,const int); //const= le paramètre du constructeur ne sera pas modifié
+		/*!
+		 *  \brief Constructeur
+		 *
+		 *  Constructeur de la classe Domino
+		 *
+		 *  \param Valeur inférieure présente sur le domino
+		 *  \param Valeur supérieure présente sur le domino
+		 *
+		 */
+		Domino(const int,const int); //const = le paramètre du constructeur ne sera pas modifié
+
+		/*!
+		 * \brief Permet d'accéder à la valeur inférieure du domino
+		 *
+		 * \return la valeur inférieure du domino
+		 */
 		int getValInf() const;
+
+		/*!
+		 * \brief Permet de modifier la valeur inférieure du domino
+		 *
+		 * \param nouvelle valeur inférieure du domino
+		 */
 		void setValInf(const int);
+
+		/*!
+		 * \brief Permet d'accéder à la valeur supérieure du domino
+		 *
+		 * \return la valeur supérieure du domino
+		 */
 		int getValSup() const;
+
+		/*!
+		 * \brief Permet de modifier la valeur supérieure du domino
+		 *
+		 * \param nouvelle valeur supérieure du domino
+		 */
 		void setValSup(const int);
+
+		/*!
+		 * \brief Permet d'afficher le domino sous la forme [nb1;nb2]
+		 */
 		friend ostream& operator<<(ostream&, const Domino&);
 };
 
--- dominos/src/Pioche.hpp
+++ dominos/src/Pioche.hpp
@@ -8,21 +8,67 @@
 #include "Domino.hpp"
 #include <vector>
 
+/*!
+ * \file Pioche.hpp
+ * \brief Pioche de dominos
+ * \author Jessie Levacher
+ */
+
 #ifndef SRC_PIOCHE_HPP_
 #define SRC_PIOCHE_HPP_
 
 using namespace std;
 
+/*!
+ * \class Pioche
+ * \brief Classe représentant la pioche du jeu du domino
+ *
+ *  La classe gère la pioche de dominos
+ */
+
 class Pioche{
+
 private:
-	static int nbDominos;
-	vector<Domino>* dominosPioche;
+	static int nbDominos; /*!< Nombre de dominos présents dans la pioche*/
+	vector<Domino>* dominosPioche; /*!< Liste des dominos dans la pioche*/
 
 public:
+	/*!
+	 *  \brief Constructeur
+	 *
+	 *  Constructeur de la classe Pioche
+	 *
+	 */
 	Pioche();
+
+	/*!
+	 * \brief Permet d'accéder au nombre de dominos
+	 *
+	 * \return le nombre de dominos
+	 */
 	int getNbDominos();
+
+	/*!
+	 * \brief Permet de modifier le nombre de dominos présent dans la pioche
+	 *
+	 * \param nouvelle quantité de dominos
+	 */
 	void setNbDominos(int);
+
+	/*!
+	 * \brief Permet d'accéder à la liste des dominos de la pioche
+	 *
+	 * \return la liste des dominos
+	 */
 	vector<Domino>* getDominosPioche();
+
+	/*!
+	 * \brief Retrait d'un domino de la pioche
+	 *
+	 * Cette méthode permet de piocher, c'est-à-dire d'enlever aléatoirement un domino de la pioche
+	 *
+	 * \return le domino pioché
+	 */
 	Domino retirerDominoPioche();
 };
 
